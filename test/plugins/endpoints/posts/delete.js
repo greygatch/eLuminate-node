cab6e6fe708c67c7d19f92d8ef0f62bc765e6621
Evan Gatchell <greygatch@gmail.com>
2015-05-27 14:02:13 -0700

    delete working

--- test/plugins/endpoints/posts/delete.js
+++ test/plugins/endpoints/posts/delete.js
@@ -21,7 +21,7 @@ var after = lab.after;
 
 var server;
 
-describe('GET /posts/{postId}', function(){
+describe('DELETE /posts/{userId}/destroy', function(){
   before(function(done){
     Server.init(function(err, srvr){
       if(err){ throw err; }
@@ -40,24 +40,24 @@ describe('GET /posts/{postId}', function(){
       Mongoose.disconnect(done);
     });
   });
-  it('should return post by user', function(done){
-    server.inject({method: 'GET', url: '/posts/b00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-  it('should return an error if bad credentials', function(done){
-    server.inject({method: 'GET', url: '/posts/q00000000000000000000001', credentials: {_id: 'b00000000000000000000011'}}, function(response){
-      expect(response.payload).to.not.be.ok;
-      done();
-    });
-  });
-  it('should throw a db error', function(done){
-    var stub = Sinon.stub(Post, 'findOne').yields(new Error());
-    server.inject({method: 'GET', url: '/posts/b00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
-      expect(response.statusCode).to.equal(400);
-      stub.restore();
-      done();
-    });
-  });
+  // it('should return post by user', function(done){
+  //   server.inject({method: 'GET', url: '/posts/b00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
+  //     expect(response.statusCode).to.equal(200);
+  //     done();
+  //   });
+  // });
+  // it('should return an error if bad credentials', function(done){
+  //   server.inject({method: 'GET', url: '/posts/q00000000000000000000001', credentials: {_id: 'b00000000000000000000011'}}, function(response){
+  //     expect(response.payload).to.not.be.ok;
+  //     done();
+  //   });
+  // });
+  // it('should throw a db error', function(done){
+  //   var stub = Sinon.stub(Post, 'findOne').yields(new Error());
+  //   server.inject({method: 'GET', url: '/posts/b00000000000000000000001', credentials: {_id: 'b00000000000000000000001'}}, function(response){
+  //     expect(response.statusCode).to.equal(400);
+  //     stub.restore();
+  //     done();
+  //   });
+  // });
 });
